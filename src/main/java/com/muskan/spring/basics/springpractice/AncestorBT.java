db282a3674431cb63cbfcc9c7dc2188fb7acaa87
muskan gupta <muskan92.nitc@gmail.com>
2020-01-13 20:16:28 +0530

    new class added

--- src/main/java/com/muskan/spring/basics/springpractice/AncestorBT.java
+++ src/main/java/com/muskan/spring/basics/springpractice/AncestorBT.java
@@ -2,6 +2,7 @@ package com.muskan.spring.basics.springpractice;
 
 public class AncestorBT {
 
+    
     BTNode root;
     BTNode ancestr;
     int found = 0;
---
+++ src/main/java/com/muskan/spring/basics/springpractice/LevelOrder.java
@@ -0,0 +1,4 @@
+package com.muskan.spring.basics.springpractice;
+
+public class LevelOrder {
+}
